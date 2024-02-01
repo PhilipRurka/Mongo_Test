519d5e931537d0df64f417906798c65b45bb5faa
philiprurka <hey@philiprurka.com>
2024-02-01 14:29:36 -0500

    Initial Project

---
+++ MongoDB-Playgrounds/TestBed-2_Sandbox.mongodb.js
@@ -0,0 +1,5 @@
+use('Staging')
+
+db.firstCollection.deleteOne({
+  _id: ObjectId('65bab02e247fe93a4fb3e6c1')
+})
---
+++ MongoDB-Playgrounds/TestBed-2_Staging_firstCollection_validation.mongodb.js
@@ -0,0 +1,25 @@
+use('Staging')
+
+db.runCommand({
+  collMod: "firstCollection",
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      title: "Updated FirstCollection Object Validation",
+      required: [ "x" ],
+      properties: {
+        x: {
+          bsonType: "int",
+          minimum: 0,
+          maximum: 10,
+          description: "'x' must be an integer between 0 & 10 and is required"
+        },
+        y: {
+          bsonType: "string",
+          description: "'y' must be a string"
+        },
+      }
+    }
+  },
+  validationAction: "error"
+});
