0c389bddf3533842baf0c833af78291c43b09717
Philip Rurka <hey@philiprurka.com>
2024-02-02 17:14:17 -0500

    more

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const config = {
+  formatOnSave: true,
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tailwindFunctions: ['clsx'],
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+
+module.exports = config
